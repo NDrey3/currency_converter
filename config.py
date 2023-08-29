12c05abed7fc4b3e502bb0938713d3e956c348ef
Andrey Ermolin <109547710+NDrey3@users.noreply.github.com>
2023-08-29 16:55:30 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+
+TOKEN = "6248886713:AAHRmQB1GsuPP2uGCeOzN1asuzCdGyJ-X-Y"
+
+keys = {
+     'биткоин':'BTC',
+     'эфириум':'ETH',
+     'евро':'EUR',
+     'доллар':'USD',
+     'рубль':'RUB',
+      'йена':'JPY',
+      'юань':'CNY',
+ }
+
